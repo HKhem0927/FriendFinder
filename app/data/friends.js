7f6839e6896eb1bbb51650401dcc44c7d52d0289
Hero <hkhem927@gmail.com>
2017-02-26 13:15:54 -0500

    working for now

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendsArray = [
+  {
+    name: "Hero Khemchandani",
+    photo: "https://i.ytimg.com/vi/Dv-evNItOvY/sddefault.jpg",
+    scores: [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+  }
+];
+
+module.exports = friendsArray; 
